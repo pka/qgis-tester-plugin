79ff96ec32dc53d71c2569a7ccb8dcd3e4757bca
volaya <volayaf@gmail.com>
2016-01-25 10:51:46 +0100

    fixed adding lesson

--- lessons/examplelessons/exporttogeojson/__init__.py
+++ lessons/examplelessons/exporttogeojson/__init__.py
@@ -1,6 +1,7 @@
 from lessons.lesson import Lesson, Step
 from lessons.utils import *
 from qgis.utils import iface
+from lessons import addLessonModule
 
 def isLayerActive():
     layer = iface.activeLayer()
@@ -15,3 +16,4 @@ lesson.addStep("Set 'points' layer as active layer", "activelayer.html",
                function = setActiveLayer, endcheck=isLayerActive, steptype=Step.MANUALSTEP)
 lesson.addMenuClickStep("Layer/Save As...")
 lesson.addStep("Save the file as geojson", "saveas.html", steptype=Step.MANUALSTEP)
+
